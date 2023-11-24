c573598e180b97426d98396344bd8dd4694b8c26
Daan <egdaan@gmail.com>
2023-11-24 21:05:49 +0100

    formatting

--- main.js
+++ main.js
@@ -1,84 +1,89 @@
 // set date
 var currentDate = new Date();
 var day = currentDate.getDate();
-var month = (currentDate.getMonth() + 1);
+var month = currentDate.getMonth() + 1;
 var year = currentDate.getFullYear();
-var formattedDate = day + "/" + month + "/" + year
-document.getElementById("currentDate").innerText = formattedDate 
+var formattedDate = day + "/" + month + "/" + year;
+document.getElementById("currentDate").innerText = formattedDate;
 
 // get random int
-var seed = currentDate.getFullYear() * 10000 + (currentDate.getMonth() + 12) * 100 + currentDate.getDate();
+var seed =
+  currentDate.getFullYear() * 10000 +
+  (currentDate.getMonth() + 21) * 100 +
+  currentDate.getDate();
 function getRandomInt(min, max) {
-    var x = Math.sin(seed++) * 10000;
-    return Math.floor((x - Math.floor(x)) * (max - min + 1)) + min;
+  var x = Math.sin(seed++) * 10000;
+  return Math.floor((x - Math.floor(x)) * (max - min + 1)) + min;
 }
 
 // Replace Response
 var confirmOutput = [
-    "Success!",
-    "Perfect!",
-    "Bullseye!",
-    "On target!",
-    "Achieved!",
-    "Excellent!",
-    "Bravo!",
-    "Superb!",
-    "Impressive!",
-    "Magnificent!",
-    "Aces!"
-  ];
+  "Success!",
+  "Perfect!",
+  "Bullseye!",
+  "On target!",
+  "Achieved!",
+  "Excellent!",
+  "Bravo!",
+  "Superb!",
+  "Impressive!",
+  "Magnificent!",
+  "Aces!",
+  "Yea!",
+];
 
 var denyOutput = [
-    "No!",
-    "Oops!",
-    "Missed it!",
-    "Not quite!",
-    "Close, but no cigar!",
-    "Not this time!",
-    "Next time for sure!",
-    "Just shy of it!",
-    "Almost nailed it!"
-  ];
+  "No!",
+  "Oops!",
+  "Missed it!",
+  "Not quite!",
+  "Close, but no cigar!",
+  "Not this time!",
+  "Next time for sure!",
+  "Just shy of it!",
+  "Almost nailed it!",
+];
 
 var supriseChallengeOutput = [
   "Planking",
   "Burpee Blitz",
   "Mountain Climber Mayhem",
   "Lunge Explosion",
-  "Jump Rope Jam"
+  "Jump Rope Jam",
 ];
 
 // Check if variable is active & replace
 function isVarActive(variable, percentInt) {
-    var roofNumber = 100 / percentInt
-    if (getRandomInt(1, roofNumber) === 1) {
-        let confirmation = confirmOutput[getRandomInt(0, confirmOutput.length - 1)]
+  var roofNumber = 100 / percentInt;
+  if (getRandomInt(1, roofNumber) === 1) {
+    let confirmation = confirmOutput[getRandomInt(0, confirmOutput.length - 1)];
 
-        let targetElement = document.getElementById(variable)
-        targetElement.textContent = confirmation
-        targetElement.style.color = "#27E312"
-    }
-    else {
-    let deny = denyOutput[getRandomInt(0, denyOutput.length - 1)]
-    let targetElement = document.getElementById(variable)
-    targetElement.textContent = deny
-    }
+    let targetElement = document.getElementById(variable);
+    targetElement.textContent = confirmation;
+    targetElement.style.color = "#27E312";
+  } else {
+    let deny = denyOutput[getRandomInt(0, denyOutput.length - 1)];
+    let targetElement = document.getElementById(variable);
+    targetElement.textContent = deny;
+  }
 }
 
 function isChallangeActive(percentInt) {
-    var roofNumber = 100 / percentInt
-    if (getRandomInt(1, roofNumber) === 1) {
-    let challenge = supriseChallengeOutput[getRandomInt(0, supriseChallengeOutput.length - 1)]
+  var roofNumber = 100 / percentInt;
+  if (getRandomInt(1, roofNumber) === 1) {
+    let challenge =
+      supriseChallengeOutput[
+        getRandomInt(0, supriseChallengeOutput.length - 1)
+      ];
 
-    let targetElement = document.getElementById('supriseChallenge')
-    targetElement.innerHTML = `<i class="fa-solid fa-circle-exclamation"></i> ${challenge}`
-    targetElement.style.color = "#27E312"
-    }
-    else {
-      let deny = denyOutput[getRandomInt(0, denyOutput.length - 1)]
-      let targetElement = document.getElementById('supriseChallenge')
-      targetElement.innerHTML = deny
-      }
+    let targetElement = document.getElementById("supriseChallenge");
+    targetElement.innerHTML = `<i class="fa-solid fa-circle-exclamation"></i> ${challenge}`;
+    targetElement.style.color = "#27E312";
+  } else {
+    let deny = denyOutput[getRandomInt(0, denyOutput.length - 1)];
+    let targetElement = document.getElementById("supriseChallenge");
+    targetElement.innerHTML = deny;
+  }
 }
 
 isVarActive("music", 40);
